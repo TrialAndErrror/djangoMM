fbfb1f2770c5090b246d840775011a15d23af3a9
TrialAndErrror <coding@trialanderrror.com>
2021-06-23 22:25:12 -0400

    Adding allowed_host

--- mm_project/mm_project/settings.py
+++ mm_project/mm_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['https://young-peak-42669.herokuapp.com/', '127.0.0.1']
 
 # Application definition
 
