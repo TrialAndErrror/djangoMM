3de4b09b8dea75008173ca6887d7559f784e1197
Trial and Errror <coding@trialanderrror.com>
2024-11-29 14:33:40 -0500

    handle category on bulk upload

--- mm_project/expenses/views/bulk_upload.py
+++ mm_project/expenses/views/bulk_upload.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 from accounts.models import Account
 from expenses.forms import CSVUploadForm
-from expenses.models import Expense
+from expenses.models import Expense, ExpenseCategory
 
 
 @login_required
@@ -19,23 +19,24 @@ def upload_csv(request):
             decoded_file = file.read().decode('utf-8').splitlines()
             reader = csv.DictReader(decoded_file)
 
+            row: dict
             for row in reader:
                 if "-" in row['Amount']:
                     row_amount = row['Amount'].replace('-', '')
                 else:
                     row_amount = f"-{row['Amount']}"
 
+                category = ExpenseCategory.objects.get_or_create(name=row['Category'])
 
                 try:
-                    Expense.objects.update_or_create(
+                    obj, created = Expense.objects.update_or_create(
                         name=row['Description'],
                         amount=row_amount,
                         date=row['Date'],
                         owner=request.user,
                         account_id=request.POST['account'],
                         defaults={
-                            "notes": row['Original Description'],
-                            "category": row['Category'],
+                            "category": category,
                         }
                     )
                 except Expense.MultipleObjectsReturned:
@@ -43,10 +44,16 @@ def upload_csv(request):
                         {"date": row['Date'], "description": row['Description'], "amount": row['Amount']})
                     continue
 
-            messages.success(request, "CSV file successfully uploaded and processed.")
+                if created:
+                    obj.notes = row['Original Description']
 
+                obj.category = category
+                obj.save()
+
+        messages.success(request, "CSV file successfully uploaded and processed.")
 
     else:
         form = CSVUploadForm()
         form.fields['account'].queryset = Account.objects.filter(owner=request.user)
+
     return render(request, 'upload_csv.html', {'form': form, "duplicates": duplicate_entries})
