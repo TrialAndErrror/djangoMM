a4234e8251f2a897a3abff8c5bbf4968a87341ac
Trial and Errror <coding@trialanderrror.com>
2024-12-28 11:50:10 -0500

    cleanups

--- mm_project/expenses/models.py
+++ mm_project/expenses/models.py
@@ -9,6 +9,12 @@ class ExpenseCategory(models.Model):
     name = models.CharField(max_length=150, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
 
 class Expense(models.Model):
     name = models.CharField(max_length=150)
